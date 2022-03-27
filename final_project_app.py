6e415c66fa19ac0f82aba1664f36dd95971928ad
jmd543 <99136294+jmd543@users.noreply.github.com>
2022-03-27 15:22:31 -0400

    Update final_project_app.py

--- final_project_app.py
+++ final_project_app.py
@@ -52,7 +52,7 @@ else:
 # Lookup avg_workout_cals_1hr from DB
 avg_workout_cals_1hr = pd.read_csv('Average_calories_burned_1hr_v2.csv')
 st.table(avg_workout_cals_1hr)
-#avg_workout_cals_1hr.set_index('Activity')
+avg_workout_cals_1hr.set_index('Activity')
 
 #activity = 'Archery'
 #activity_interpolator = interpolate.interp1d(avg_workout_cals_1hr.columns[1:],avg_workout_cals_1hr.loc[activity,:], fill_value = 'extrapolate')
