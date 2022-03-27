d17b0a56eeafac8089da3978d6d29c5fdb3024ca
jmd543 <99136294+jmd543@users.noreply.github.com>
2022-03-27 15:28:24 -0400

    Update final_project_app.py

--- final_project_app.py
+++ final_project_app.py
@@ -54,8 +54,8 @@ avg_workout_cals_1hr = pd.read_csv('Average_calories_burned_1hr_v2.csv')
 avg_workout_cals_1hr.set_index('Activity')
 st.table(avg_workout_cals_1hr)
 
-activity = 'General'
-activity_interpolator = interpolate.interp1d(avg_workout_cals_1hr.columns[1:],avg_workout_cals_1hr.loc[activity,:], fill_value = 'extrapolate')
+Activity = 'General'
+activity_interpolator = interpolate.interp1d(avg_workout_cals_1hr.columns[1:],avg_workout_cals_1hr.loc[Activity,:], fill_value = 'extrapolate')
 avg_workout_cals = activity_interpolator(weight)
 
 type('Awesome! Now tell us what are your health goals?')
